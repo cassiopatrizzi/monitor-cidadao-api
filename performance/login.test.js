be00bd649603c4b6d5e2ebfa03c29a47917f7e9a
Cassio Patrizzi <cassio_patrizzi@yahoo.com>
2025-11-18 20:39:28 -0300

    Implemantado stages para simular carga de usuarios

--- performance/login.test.js
+++ performance/login.test.js
@@ -4,8 +4,11 @@ import { registerAndValidateUser } from './helpers/registerHelper.js';
 import { user } from './fixtures/userFixture.js';
 
 export const options = {
-    vus: 10,
-    duration: '30s',
+    stages: [
+        { duration: '5s', target: 20 },
+        { duration: '20s', target: 300 },
+        { duration: '5s', target: 0 }
+    ],
     thresholds: {
         http_req_duration: ['p(90)<3000', 'max<5000'],
         http_req_failed: ['rate<0.01']
