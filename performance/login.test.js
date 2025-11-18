025820d3c5d95bc7037d83c658bc514d1634a824
Cassio Patrizzi <cassio_patrizzi@yahoo.com>
2025-11-18 19:51:27 -0300

    Adicao de VUs e duration para execucao dos testes

--- performance/login.test.js
+++ performance/login.test.js
@@ -4,11 +4,10 @@ import { registerAndValidateUser } from './helpers/registerHelper.js';
 import { user } from './fixtures/userFixture.js';
 
 export const options = {
-    // vus: 10,
-    // duration: '30s'
-    iterations: 10,
+    vus: 10,
+    duration: '30s',
     thresholds: {
-        http_req_duration: ['p(90)<5000', 'max<3000'],
+        http_req_duration: ['p(90)<3000', 'max<5000'],
         http_req_failed: ['rate<0.01']
     },
 };
